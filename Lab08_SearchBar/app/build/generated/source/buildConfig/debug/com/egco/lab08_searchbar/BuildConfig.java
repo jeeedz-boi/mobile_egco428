4e3654ba847fc14865a4e337fde4b3f45daec7d6
Jeeedz <naiguncha@gmail.com>
2020-10-01 09:40:25 +0700

    Lab08 SearchBar Added

---
+++ Lab08_SearchBar/app/build/generated/source/buildConfig/debug/com/egco/lab08_searchbar/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.egco.lab08_searchbar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.egco.lab08_searchbar";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
